86c151e718d10213737e1c73160493105ec37fef
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-08-09 15:18:57 +0200

    hero and skills v1.0.0; started work on timeline-component

---
+++ src/utils/IconMap.ts
@@ -0,0 +1,13 @@
+// Using devicons and mdi from iconify
+export const IconMapDark = {
+  arch: "devicon-plain:archlinux",
+  dotnet: "devicon-plain:net",
+  bash: "devicon-plain:bash",
+  git: "devicon-plain:git",
+  markdown: "devicon-plain:markdown",
+  mysql: "devicon-plain:mysql",
+  nodejs: "devicon-plain:nodejs",
+  powershell: "devicon-plain:powershell",
+  react: "mdi:react",
+  astro: "devicon-plain:astro",
+};
